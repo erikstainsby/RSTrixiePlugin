fc19824824de27cc67fa3584da0740654cc2a5fb
Erik Stainsby <erik@roaringsky.local>
2012-02-15 20:10:29 -0800

    Initial Commit

---
+++ RSTrixiePlugin/RSTrixiePlugin.h
@@ -0,0 +1,13 @@
+//
+//  RSTrixiePlugin.h
+//  RSTrixiePlugin
+//
+//  Created by Erik Stainsby on 12-02-15.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RSTrixiePlugin : NSObject
+
+@end
---
+++ RSTrixiePlugin/RSTrixiePlugin.m
@@ -0,0 +1,23 @@
+//
+//  RSTrixiePlugin.m
+//  RSTrixiePlugin
+//
+//  Created by Erik Stainsby on 12-02-15.
+//  Copyright (c) 2012 Roaring Sky. All rights reserved.
+//
+
+#import "RSTrixiePlugin.h"
+
+@implementation RSTrixiePlugin
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
